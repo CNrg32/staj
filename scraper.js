63ac8b36203eddfb27bfaa281ea158c8a6906839
halil melih akça <halilmelih32@gmail.com>
2025-01-23 18:02:05 +0300

    iphonelerin hepsi bitti ama site prizeleri ve linkleri düzenlemek gerekiyor

--- scraper.js
+++ scraper.js
@@ -69,7 +69,6 @@ const pdfPath = "./Networks Bilişim - Fiyat Listesi - 21012025.pdf";
                 });
             }
         }
-
         // Sonuçları kaydet
         results.push({
             name: result.name,
@@ -104,12 +103,14 @@ const pdfPath = "./Networks Bilişim - Fiyat Listesi - 21012025.pdf";
     await browser.close();
 })();
 class Item {
-    constructor(itemId,name,price,sitePrice,link) {
-        this.id = id;
+    constructor(itemId,name,ourprice,sitePrice,link,visited,color) {
         this.link = link;
         this.sitePrice = sitePrice;
         this.name = name;
-        this.price = price;
+        this.ourprice = ourprice;
+        this.itemId=itemId;
+        this.visited = visited;
+        this.color = color;
     }
 }
 // PDF işleme fonksiyonu
@@ -151,9 +152,57 @@ async function processPdf(filePath) {
                     }
                 }
             });
+        console.log(itemsDictionary);
         return itemsDictionary;
     } catch (error) {
         console.error("PDF işleme sırasında bir hata oluştu:", error);
         return {};
     }
 }
+//bu kısmı elle yaptım çünkü eşitleme yapamıyoruz
+//iphonelerin hepsi bitti ama site prizeleri ve linkleri düzenlemek gerekiyor
+var arr = [];
+let item1 = new Item(1, "iphone 16 Pro Max 256GB", 95500, 2000, "https://www.akakce.com/arama/?q=iphone+12",0,"Siyah");
+let item2 = new Item(2, "iphone 16 Pro 128GB", 82000, 2000, "https://www.akakce.com/arama/?q=iphone+11",0,"Çöl Titanyum");
+let item3 = new Item(3, "iphone 16 128GB", 58500, 2000, "https://www.akakce.com/arama/?q=iphone+10",0,"Lacivert");
+let item4 = new Item(4, "iphone 15 128GB", 51600, 2000, "https://www.akakce.com/arama/?q=iphone+9",0,"Siyah");
+let item5 = new Item(5, "iphone 14 128GB", 42800, 2000, "https://www.akakce.com/arama/?q=iphone+8",0,"Siyah");
+arr.push(item1);
+arr.push(item2);
+arr.push(item3);
+arr.push(item4);
+arr.push(item5);
+let item6 = new Item(6, "iphone 13 128GB", 34800, 2000, "https://www.akakce.com/arama/?q=iphone+7",0,"Siyah");
+arr.push(item6);
+let item7 = new Item(7, "iphone 12 256GB", 33400, 2000, "https://www.akakce.com/arama/?q=iphone+6", 0,"Kırmızı");
+arr.push(item7);
+let item8 = new Item(8, "iphone 12 128GB", 33000, 2000, "https://www.akakce.com/arama/?q=iphone+5",0,"Mor");
+arr.push(item8);
+let item9 = new Item(9, "iphone 12 64GB", 29300, 2000, "https://www.akakce.com/arama/?q=iphone+4", 0,"Kırmızı");
+arr.push(item9);
+let item10 = new Item(10, "iphone 12 64GB", 29300, 2000, "https://www.akakce.com/arama/?q=iphone+3",0,"Mor");
+arr.push(item10);
+let item11 = new Item(11, "iphone 12 64GB", 29300, 2000, "https://www.akakce.com/arama/?q=iphone+2", 0,"Beyaz");
+arr.push(item11);
+let item12 = new Item(12, "iphone 12 64GB", 29300, 2000, "https://www.akakce.com/arama/?q=iphone+2", 0, "Siyah");
+arr.push(item11);
+let item13 = new Item(13, "iphone 11 128GB", 28500, 2000, "https://www.akakce.com/arama/?q=iphone+1",0,"Siyah");
+arr.push(item13);
+let item14 = new Item(14, "iphone 11 64GB", 25000, 2000, "https://www.akakce.com/arama/?q=iphone+1",0,"Siyah");
+arr.push(item14);
+let item15 = new Item(15, "iPad Mini 6.Nesil+Cellular (Sim Kartlı) 256GB", 27000, 2000, "https://www.akakce.com/arama/?q=iphone+1", 0,"Beyaz");
+arr.push(item15);
+let item16 = new Item(16, "iPad Mini 8.3'(6.Nesil) 64GB", 19500, 2000, "https://www.akakce.com/arama/?q=iphone+1", 0,"Beyaz");
+arr.push(item16);
+//'Airpods Pro 2 (Type-C)Beyaz': 9800, 'Airpods 4 (ANC)Beyaz': 8800,'Airpods 4Beyaz': 6700,'Airpods 3 (MagSafe)Beyaz': 6700,'Airpods 2Beyaz': 5200,
+let item17 = new Item(17, "Airpods Pro 2 (Type-C)", 9800, 2000, "https://www.akakce.com/arama/?q=iphone+1", 0,"Beyaz");
+arr.push(item17);
+let item18 = new Item(18, "Airpods 4 (ANC)", 8800, 2000, "https://www.akakce.com/arama/?q=iphone+1", 0,"Beyaz");
+arr.push(item18);
+let item19 = new Item(19, "Airpods 4", 6700, 2000, "https://www.akakce.com/arama/?q=iphone+1", 0,"Beyaz");
+arr.push(item19);
+let item20 = new Item(20, "Airpods 3 (MagSafe)", 6700, 2000, "https://www.akakce.com/arama/?q=iphone+1", 0,"Beyaz");
+arr.push(item20);
+let item21 = new Item(21, "Airpods 2", 5200, 2000, "https://www.akakce.com/arama/?q=iphone+1", 0,"Beyaz");
+arr.push(item21);
+
