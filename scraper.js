14bd7fb80dded3769511945d61911d48f1d2bc34
Emir Dizdar <157601831+mdizdar53@users.noreply.github.com>
2025-02-10 12:39:12 +0300

    yeni apiden ürünler çekiliyor

--- scraper.js
+++ scraper.js
@@ -4,9 +4,57 @@ const fs = require("fs");
 puppeteer.use(StealthPlugin());
 const nodemailer = require("nodemailer");
 const readline = require('readline');
-
-
+const pdf = require('pdf-parse');
 const { downloadLatestPdf } = require('./downloadPdfFromGmail'); // Gmail'den PDF alma fonksiyonunu içe aktar
+
+
+const axios = require('axios');
+const { storage } = require('googleapis/build/src/apis/storage');
+
+
+class Item {
+    constructor(id, name, buyPrice, sitePrice, link, storageName, colorName, upgradedPrice, stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.buyPrice = buyPrice;
+        this.sitePrice = sitePrice;
+        this.link = link;
+        this.storageName = storageName;
+        this.colorName = colorName;
+        this.upgradedPrice = upgradedPrice;  // Bu satırda upgradedPrice doğru şekilde atanmalı
+        this.stockQuantity = stockQuantity;  // Bu satırda stockQuantity doğru şekilde atanmalı
+    }
+}
+
+// API'den ürün verilerini çekme
+async function fetchProductData() {
+    try {
+        const response = await axios.get('https://networksadmin.netliste.com/api/getProductListWithAkakceURL');
+        const products = response.data;
+
+        // Her ürün için Item objesi oluştur
+        const items = products.map((product, index) => {
+            return new Item(
+                product.productID,                // id
+                product.productName,              // name
+                parseFloat(product.buyPrice),     // buyPrice
+                0,                                // sitePrice
+                product.akakceURL,                // link
+                product.storageName,              // storage
+                product.colorName,                // color
+                0,                                // upgradedPrice (başlangıçta 0, daha sonra güncellenebilir)
+                product.stockQuantity             // stockQuantity
+            );
+        });
+
+        console.log(items); // Item objelerini konsola yazdır
+        return items;
+    } catch (error) {
+        console.error('Veri çekme hatası:', error.message);
+    }
+}
+
+// API'den veri çek ve Item objeleri oluştur
 
 
 let count = 0;
@@ -15,48 +63,9 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Item sınıfı
-class Item {
-    constructor(id, name, price, sitePrice,upgradedPrice, link) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.sitePrice = sitePrice;
-        this.link = link;
-        this.upgradedPrice = upgradedPrice;
-    }
-}
-
-// Dosyadan linkleri oku
-const fileContent = fs.readFileSync('./link.txt', 'utf-8');
-const links = fileContent.match(/https?:\/\/[^\s]+/g); // Tüm linkleri çıkar
 
 (async () => {
-    console.log('Gmail’den PDF indiriliyor...');
-    const downloadedPdfPath = await downloadLatestPdf(); // Gmail'den en son PDF dosyasını al
     
-    if (!downloadedPdfPath) {
-        console.log('PDF bulunamadı, işlem sonlandırılıyor.');
-        return;
-    }
-
-    console.log(`İndirilen PDF: ${downloadedPdfPath}`);
-
-    let myPrices = {}; // `myPrices` sözlüğünü burada tanımlıyoruz.
-
-    try {
-        // PDF işleme
-        myPrices = await processPdf(downloadedPdfPath); // PDF içeriği işlendi
-        // Sözlüğün boyutunu öğrenmek
-const size = Object.keys(myPrices).length;
-console.log(`Sözlükteki eleman sayısı: ${size}`);
-        console.log('PDF başarıyla işlendi:', myPrices);
-        console.log(myPrices.length)
-    } catch (error) {
-        console.error('PDF işlenirken hata oluştu:', error);
-        return;
-    }
-
     // Tarayıcı başlatma
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -64,20 +73,22 @@ console.log(`Sözlükteki eleman sayısı: ${size}`);
     await page.setExtraHTTPHeaders({ 'Accept-Language': 'tr' });
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 OPR/115.0.0.0');
 
-    const items = []; // Tüm ürün objelerini saklamak için array
+    const items = await fetchProductData(); // Tüm ürün objelerini saklamak için array
     const anomalies = []; // Anormallik bulunan ürünler
     let id = 1; // ID sırası
     let index = 0; // MyPrices sözlüğü için bir sayaç
 
+
     // Anormallik kontrol ve e-posta bildirimi
-    for (const link of links) {
+    for (const item of items) {
+        const link = item.link;
         console.log(`"${link}" için bilgi alınıyor...`);
 
 
-        const keys = Object.keys(myPrices);
-console.log("Sözlükteki Key'ler:", keys);
+        
         await page.goto(link, { waitUntil: "networkidle2" });
-        console.log(myPrices)
+
+        // Akakçe sitesinden fiyat çekme
         const result = await page.evaluate(() => {
             function extractPrice(text) {
                 return parseFloat(text.replace(/\./g, '').replace(',', '.')) || 0;
@@ -118,36 +129,20 @@ console.log("Sözlükteki Key'ler:", keys);
             // İlk 5 fiyatın ortalamasını al
             const averagePrice = firstFivePrices.reduce((sum, price) => sum + price, 0) / firstFivePrices.length;
 
-
-            /*// 4️⃣ Ortalama ve standart sapma hesaplama
-            const mean = prices.reduce((sum, val) => sum + val, 0) / prices.length;
-            const variance = prices.reduce((sum, val) => sum + Math.pow(val - mean, 2), 0) / prices.length;
-            const stdDev = Math.sqrt(variance);
-
-            // 5️⃣ Güvenilir fiyat aralığını belirle (ortalama ± 1 standart sapma)
-            const minThreshold = mean - stdDev;
-            const maxThreshold = mean + stdDev;
-
-            // 6️⃣ Uç değerleri çıkarıp yeni ortalama al
-            const filteredPrices = prices.filter(price => price >= minThreshold && price <= maxThreshold);
-            const finalPrice = filteredPrices.length > 0
-                ? filteredPrices.reduce((sum, val) => sum + val, 0) / filteredPrices.length
-                : mean; // Eğer filtrelenen fiyat yoksa, orijinal ortalama kullan
-            */
             return {
                 name,
                 sitePrice: averagePrice // Ortalama fiyatı döndür
             };
-            //fazladan bir fiyat daha vardı onu kaldırdım
-
-            
         });
-        const myPriceKeys = Object.keys(myPrices);
-        const currentKey = myPriceKeys[index];
-        const myPrice = myPrices[currentKey] || 0;//değiştirmem gerektiği için let yaptım
 
-        console.log(`MyPrices Key: ${currentKey}, Fiyat: ${myPrice}`);
+        item.sitePrice = result.sitePrice; // averagePrice
+
+
+        
+        let myPrice = item.buyPrice || 0; // buyPrice'ı kullanıyoruz, item objesindeki buyPrice
+
         let upgradedPrice = myPrice;
+
         if (myPrice > 70000) {
             upgradedPrice = myPrice - (myPrice * 0.01);
         } else if (myPrice > 50000) {
@@ -160,11 +155,9 @@ console.log("Sözlükteki Key'ler:", keys);
         } else {
             upgradedPrice = myPrice - (myPrice * 0.10);
         }
-        const item = new Item(id, result.name, myPrice, result.sitePrice,upgradedPrice, link);
-
-        items.push(item);
         
-        if (myPrice > 0 && result.sitePrice > 0) {
+        
+        if (myPrice > 0 && item.sitePrice > 0) {
             // 70 den fazlaysa yüzde 1 50 den fazlaysa yüzde 2 30 dan fazlaysa yüzde 3 10 dan fazlaysa yüzde 5 farkı takip et
             //çıkaracak şekilde yaptım çünkü kontrolü çıkarılmış üstünden yapıcaz
             if(myPrice>70000){
@@ -180,104 +173,84 @@ console.log("Sözlükteki Key'ler:", keys);
                 upgradedPrice = myPrice - (myPrice * 0.10);
             }
 
-            const difference = Math.abs(myPrice - result.sitePrice);
+            const difference = Math.abs(myPrice - item.sitePrice);
             const percentageDifference = (difference / upgradedPrice) * 100;
 
-            
-            //eğer fiyat aralığı 70 den büyükse yüzde 2 fark takib et
-            if (percentageDifference > 2 && upgradedPrice >70000) {
-                anomalies.push({
-                    name: result.name,
-                    myPrice: myPrice,
-                    sitePrice: result.sitePrice,
-                    upgradedPrice: upgradedPrice,
-                    percentageDifference: percentageDifference.toFixed(2),
-                    link: link // Doğru şekilde link'i atıyoruz
-                });
 
+anomalies.push({
+    name: item.name,
+    myPrice: myPrice,
+    sitePrice: item.sitePrice,
+    upgradedPrice: upgradedPrice,
+    percentageDifference: percentageDifference.toFixed(2),
+    link: item.link,
+    storageName: item.storageName,
+    colorName: item.colorName,
+    stockQuantity: item.stockQuantity
+});
 
-                // E-posta gönderimi
-                const subject = `Anormallik Tespiti: ${result.name}`;
-                const message = `
-Ürün: ${result.name}
-Bizim Fiyatımız: ${myPrice} TL
-anormallik hesapladığımız Fiyat: ${upgradedPrice} TL
-Site Fiyatı: ${result.sitePrice} TL
-Fark (%): ${percentageDifference.toFixed(2)}%
-Link: ${link}
-            `;
-                //await sendEmail(subject, message); // E-posta gönder
-            }//eğer fiyat aralığı 70 den küçükse ve 50 den büyükse yüzde 5 fark takib et
-            else if (percentageDifference > 5 && upgradedPrice >50000) {
-                anomalies.push({
-                    name: result.name,
-                    myPrice: myPrice,
-                    sitePrice: result.sitePrice,
-                    percentageDifference: percentageDifference.toFixed(2),
-                    link: link
-                });
-
-
-                // E-posta gönderimi
-                const subject = `Anormallik Tespiti: ${result.name}`;
-                const message = `
-Ürün: ${result.name}
-Bizim Fiyatımız: ${myPrice} TL
-anormallik hesapladığımız Fiyat: ${upgradedPrice} TL
-Site Fiyatı: ${result.sitePrice} TL
-Fark (%): ${percentageDifference.toFixed(2)}%
-Link: ${link}
-            `;
-                //await sendEmail(subject, message); // E-posta gönder
-            }//eğer fiyat aralığı 50 den küçükse ve 30 dan büyükse yüzde 7 fark takib et
-            else if (percentageDifference > 7 && upgradedPrice > 30000) {
-                anomalies.push({
-                    name: result.name,
-                    myPrice: myPrice,
-                    sitePrice: result.sitePrice,
-                    upgradedPrice: upgradedPrice,
-                    percentageDifference: percentageDifference.toFixed(2),
-                    link: link
-                });
-
-
-                // E-posta gönderimi
-                const subject = `Anormallik Tespiti: ${result.name}`;
-                const message = `
-Ürün: ${result.name}
-Bizim Fiyatımız: ${myPrice} TL
-anormallik hesapladığımız Fiyat: ${upgradedPrice} TL
-Site Fiyatı: ${result.sitePrice} TL
-Fark (%): ${percentageDifference.toFixed(2)}%
-Link: ${link}
-            `;
-                //await sendEmail(subject, message); // E-posta gönder
-            }//eğer fiyat aralığı 30 dan küçükse ve 10 dan büyükse yüzde 10 fark takib et
-            else if (percentageDifference > 10 && upgradedPrice > 10000) {
-                anomalies.push({
-                    name: result.name,
-                    myPrice: myPrice,
-                    sitePrice: result.sitePrice,
-                    upgradedPrice: upgradedPrice,
-                    percentageDifference: percentageDifference.toFixed(2),
-                    link: link
-                });
-
-
-                // E-posta gönderimi
-                const subject = `Anormallik Tespiti: ${result.name}`;
-                const message = `
-Ürün: ${result.name}
-Bizim Fiyatımız: ${myPrice} TL
-anormallik hesapladığımız Fiyat: ${upgradedPrice} TL
-Site Fiyatı: ${result.sitePrice} TL
-Fark (%): ${percentageDifference.toFixed(2)}%
-Link: ${link}
-            `;
-                //await sendEmail(subject, message); // E-posta gönder
-            }
-
-
+            /*
+           // Fiyat farkına göre anormallik kontrolleri
+if (myPrice > 0 && item.sitePrice > 0) {
+    // 70.000 TL'yi aşan ürünler için %2 fark
+    if (percentageDifference > 2 && upgradedPrice > 70000) {
+        anomalies.push({
+            name: item.name,
+            myPrice: myPrice,
+            sitePrice: item.sitePrice,
+            upgradedPrice: upgradedPrice,
+            percentageDifference: percentageDifference.toFixed(2),
+            link: item.link,
+            storageName: item.storageName,
+            colorName: item.colorName,
+            stockQuantity: item.stockQuantity
+        });
+    } 
+    // 50.000 TL'yi aşan ürünler için %5 fark
+    else if (percentageDifference > 5 && upgradedPrice > 50000) {
+        anomalies.push({
+            name: item.name,
+            myPrice: myPrice,
+            sitePrice: item.sitePrice,
+            upgradedPrice : item.upgradedPrice,
+            percentageDifference: percentageDifference.toFixed(2),
+            link: item.link,
+            storageName: item.storageName,
+            colorName: item.colorName,
+            stockQuantity: item.stockQuantity
+        });
+    } 
+    // 30.000 TL'yi aşan ürünler için %7 fark
+    else if (percentageDifference > 7 && upgradedPrice > 30000) {
+        anomalies.push({
+            name: item.name,
+            myPrice: myPrice,
+            sitePrice: item.sitePrice,
+            upgradedPrice: upgradedPrice,
+            percentageDifference: percentageDifference.toFixed(2),
+            link: item.link,
+            storageName: item.storageName,
+            colorName: item.colorName,
+            stockQuantity: item.stockQuantity
+        });
+    }
+    // 10.000 TL'yi aşan ürünler için %10 fark
+    else if (percentageDifference > 10 && upgradedPrice > 10000) {
+        anomalies.push({
+            name: item.name,
+            myPrice: myPrice,
+            sitePrice: item.sitePrice,
+            upgradedPrice: upgradedPrice,
+            percentageDifference: percentageDifference.toFixed(2),
+            link: item.link,
+            storageName: item.storageName,
+            colorName: item.colorName,
+            stockQuantity: item.stockQuantity
+        });
+    }
+        
+}
+*/
         }
 
         index++;
@@ -286,52 +259,63 @@ Link: ${link}
         await sleep(Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000);
     }
 
-
     console.log('Ürünler oluşturuldu:', items);
-    // Anormallikleri toplu e-posta ile gönder
-    if (anomalies.length > 0) {
-        const anomalyReport = anomalies.map(item => {
-            return `Anormallik: ${item.name}\n` +
-                `  Bizim Fiyatımız: ${item.myPrice} TL\n` +
-                `  Site Fiyatı: ${item.sitePrice} TL\n` +
-                `  Fark (%): ${item.percentageDifference}%\n` +
-                `  Link: ${item.link}\n`; // Link'i ekliyoruz
 
-        }).join('\n');
+// Anormallikleri toplu e-posta ile gönder
+if (anomalies.length > 0) {
+    // Her bir anormallik içeren ürün için tüm bilgileri ekle
+    const anomalyReport = anomalies.map(item => {
+        return `Anormallik: ${item.name}\n` +
+            `  Bizim Fiyatımız: ${item.myPrice} TL\n` +
+            `  Site Fiyatı: ${item.sitePrice} TL\n` +
+            `  Güncellenmiş Fiyat: ${item.upgradedPrice} TL\n` +  // upgradedPrice bilgisi
+            `  Fark (%): ${item.percentageDifference}%\n` +
+            `  Link: ${item.link}\n` +  // Link'i ekliyoruz
+            `  Depolama: ${item.storageName}\n` +  // Storage bilgisini ekliyoruz
+            `  Renk: ${item.colorName}\n` +  // Color bilgisini ekliyoruz
+            `  Stok Miktarı: ${item.stockQuantity}\n` +  // StockQuantity bilgisini ekliyoruz
+            `  Raporlanma Zamanı: ${new Date().toLocaleString()}\n\n`; // Raporlanma zamanını ekliyoruz
+    }).join('\n');
+    const subject = "Anormallik Tespit Raporu";
+    const message = `Aşağıdaki ürünlerde anormallik tespit edilmiştir:\n\n${anomalyReport}`;
+    await sendEmail(subject, message); // E-posta gönder
+} else {
+    const subject = "Anormallik Tespit Raporu";
+    const message = "Hiçbir anormallik tespit edilmemiştir.";
+    await sendEmail(subject, message); // E-posta gönder
+}
 
-        const subject = "Anormallik Tespit Raporu";
-        const message = `Aşağıdaki ürünlerde anormallik tespit edilmiştir:\n\n${anomalyReport}`;
-        await sendEmail(subject, message); // E-posta gönder
-    } else {
-        const subject = "Anormallik Tespit Raporu";
-        const message = "Hiçbir anormallik tespit edilmemiştir.";
-        await sendEmail(subject, message); // E-posta gönder
-    }
+// Objeleri bir dosyaya yazdır
+fs.writeFileSync('items.json', JSON.stringify(items, null, 2), 'utf-8');
+console.log('Ürün objeleri items.json dosyasına kaydedildi.');
+// Anormallikleri anormal.txt dosyasına yazdır
+if (anomalies.length > 0) {
+    const anomalyReport = anomalies.map(item => {
+        return `Anormallik: ${item.name}\n` +
+            `  Site Fiyatı: ${item.sitePrice} TL\n` +
+            `  Bizim Fiyatımız: ${item.myPrice} TL\n` +
+            `  Anormallik Hesapladığımız Fiyat: ${item.upgradedPrice} TL\n` +
+            `  Fark (%): ${item.percentageDifference}%\n` +
+            `  Link: ${item.link}\n` +   // Link'i de ekliyoruz
+            `  Depolama: ${item.storageName}\n` + // Depolama bilgisini ekliyoruz
+            `  Renk: ${item.colorName}\n` + // Renk bilgisini ekliyoruz
+            `  Stok Miktarı: ${item.stockQuantity}\n` + // Stok miktarını ekliyoruz
+            `  ---------------------------------------------\n`;  // Satır sonu ekliyoruz
+    }).join('\n');
+    
+    // Anormallik raporunu anormal.txt dosyasına kaydet
+    fs.writeFileSync('anormal.txt', anomalyReport, 'utf-8');
+    console.log('Anormallikler anormal.txt dosyasına kaydedildi.');
+} else {
+    console.log('Anormallik tespit edilmedi.');
+}
 
-    // Objeleri bir dosyaya yazdır
-    fs.writeFileSync('items.json', JSON.stringify(items, null, 2), 'utf-8');
-    console.log('Ürün objeleri items.json dosyasına kaydedildi.');
 
-    // Anormallikleri anormal.txt dosyasına yazdır
-    if (anomalies.length > 0) {
-        const anomalyReport = anomalies.map(item => {
-            return `Anormallik: ${item.name}\n` +
-                `  Site Fiyatı: ${item.sitePrice} TL\n` +
-                `  Bizim Fiyatımız: ${item.myPrice} TL\n` +
-                `  anormallik hesapladığımız Fiyat: ${item.upgradedPrice} TL\n` +
-                `  Fark (%): ${item.percentageDifference}%\n`;
-        }).join('\n');
-        fs.writeFileSync('anormal.txt', anomalyReport, 'utf-8');
-        console.log('Anormallikler anormal.txt dosyasına kaydedildi.');
-    } else {
-        console.log('Anormallik tespit edilmedi.');
-    }
 
     await browser.close();
+
+
 })();
-
-const pdf = require('pdf-parse');
-
 
 // PDF işleme fonksiyonu
 async function processPdf(filePath) {
@@ -356,8 +340,6 @@ async function processPdf(filePath) {
     return itemsDictionary;
 }
 
-
-
 // E-posta gönderme fonksiyonu
 async function sendEmail(subject, message) {
     const transporter = nodemailer.createTransport({
@@ -373,7 +355,9 @@ async function sendEmail(subject, message) {
     const mailOptions = {
         from: "networksnotification@gmail.com",
         to: "emirxdizdar@gmail.com",
-        //to: "mfatihgumus@networksbilisim.com", //fatih abinin mail adresi
+        to: "networksnotification@gmail.com",
+        to: "mfatihgumus@networksbilisim.com", 
+        to: "ayberkozkaya@hotmail.com",
         subject: subject,
         text: message
     };
